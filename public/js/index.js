2c9ad68de626c37ace8c6c6a7f665e9df73475c3
Venkatasubramanian Ramachandran <vramachandran@walmartlabs.com>
2015-09-30 14:26:51 -0700

    checkedin HTML files

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,8 @@
 let $ = require('jquery')
 let io = require('socket.io-client')
 let socket = io('http://127.0.0.1:8000')
-let $template = $('#template')
+//let $template = $('#template')
+let $template = $('table#stocktable')
 socket.on('connect', ()=>console.log('connected', socket.id))
 
 // Enable the form now that our code has loaded
@@ -13,4 +14,15 @@ $('#send').removeAttr('disabled')
 
 socket.on('stock-updates', ({watchlist})=> {
   console.log('client receivvvveeeeeeed', watchlist)
+  let $table = $template.clone().show()
+  for( let i = 0; i < watchlist.length; i++) { 
+  	let stock = watchlist[i]; 
+  	console.log("i ", i ," stock.price: ", watchlist[i].lastTradePriceOnly)
+  	console.log("debug.... ", $table.find('lastTradePrice' + i))
+  	$table.find('#symbol' + i).html(stock.symbol);
+  	$table.find('#name' + i).html(stock.name);
+  	//$table.find('lastTradePrice' + i).text(watchlist[i].lastTradePriceOnly);
+  	document.getElementById('lastTradePrice' + i) .innerHTML = stock.lastTradePriceOnly 
+  							(stock.changeInPercent.toFixed(4));
+  }
 })
