5328308c74d240f5889c1249fca1c10f198f496e
Duy Le <dle@walmartlabs.com>
2015-09-30 17:08:36 -0700

    add new feature

--- app/lib/stock.js
+++ app/lib/stock.js
@@ -7,11 +7,32 @@ require('songbird');
 async function fetchStockQuotes(email) {
   let quotes = []
   let stocks = await Stock.promise.find({ email : email});
+  let stockMap = {}
+  stocks.forEach(stock=>{
+    stockMap[stock.symbol] = stock
+  })
   if (stocks && stocks.length > 0){
     let stocklist = stocks.map( stock =>{
       return stock.symbol
     })
     quotes = await lookupTicker(stocklist)
+    quotes = quotes.map( quote =>{
+      let s = stockMap[quote.symbol]
+      let gain = (s.purchasePrice - quote.lastTradePriceOnly) / s.purchasePrice * 100
+      return {
+        symbol: quote.symbol,
+        name: quote.name,
+        lastTradeDate: quote.lastTradeDate,
+        lastTradePriceOnly: quote.lastTradePriceOnly,
+        changeInPercent: quote.changeInPercent,
+        dividendYield: quote.dividendYield,
+        peRatio: quote.peRatio,
+        change: quote.change,
+        purchasePrice: s.purchasePrice,
+        qty: s.qty,
+        gains: gain
+      }
+    })
   }
 
   return quotes
@@ -24,4 +45,12 @@ async function lookupTicker(symbols){
     })
 }
 
-module.exports = {fetchStockQuotes,lookupTicker}
+async function isValidTicker(symbol){
+    let [valid] = await lookupTicker([symbol])
+    if (valid.name){
+      return true
+    }
+    return false
+}
+
+module.exports = {fetchStockQuotes,isValidTicker}
